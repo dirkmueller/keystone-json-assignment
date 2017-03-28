01fbde33dad11a89b2430c0e1517f27a3f928f7b
Colleen Murphy <colleen.murphy@suse.com>
2017-03-28 17:56:19 +0200

    Clean up

--- keystone_json_assignment/json.py
+++ keystone_json_assignment/json.py
@@ -7,11 +7,9 @@ from oslo_log import log
 
 import keystone.conf
 from keystone.assignment.backends import sql
-from keystone.common import dependency
 from keystone.common import driver_hints
 from keystone.common import manager
 from keystone import exception
-from keystone import resource
 
 CONF = keystone.conf.CONF
 LOG = log.getLogger(__name__)
@@ -21,29 +19,61 @@ json_assignment_opts = [
                 default=['Member'],
                 help='List of roles assigned by default to an LDAP user'),
     cfg.StrOpt('ldap_domain_name',
-                default='ldap_users',
-                help='Domain for the users in the JSON map. Only supports one domain.'),
+               default='ldap_users',
+               help='Domain for the users in the JSON map.'
+               ' Only supports one domain.'),
 ]
 CONF.register_opts(json_assignment_opts, 'json_assignment')
 
+
 class Assignment(sql.Assignment):
 
-    def __init__(self):
-        self.resource_manager = manager.load_driver('keystone.resource', CONF.resource.driver)
-        domain_config = { 'cfg': cfg.ConfigOpts() }
-        keystone.conf.configure(conf=domain_config['cfg'])
-        self.domain_name = CONF.json_assignment.ldap_domain_name
-        domain_name_filter= driver_hints.Hints()
+    def _setup_managers(self):
+        self.resource_manager = manager.load_driver(
+            'keystone.resource', CONF.resource.driver)
+        self.id_mapping_manager = manager.load_driver(
+            'keystone.identity.id_mapping', CONF.identity_mapping.driver)
+        self.role_manager = manager.load_driver(
+            'keystone.role', CONF.role.driver)
+
+        # Look up the domain-specific identity driver and config
+        domain_config = cfg.ConfigOpts()
+        keystone.conf.configure(conf=domain_config)
+        domain_name_filter = driver_hints.Hints()
         domain_name_filter.add_filter('name', self.domain_name)
-        self.domain_id = self.resource_manager.get_project_by_name(self.domain_name, domain_id=None)['id']
-        domain_config_file = "/etc/keystone/domains/keystone.%s.conf" % self.domain_name
-        domain_config['cfg'](args=[], project='keystone', default_config_files=[domain_config_file], default_config_dirs=[])
-        self.identity_manager = manager.load_driver('keystone.identity', domain_config['cfg'].identity.driver, domain_config['cfg'])
-        self.id_mapping_manager = manager.load_driver('keystone.identity.id_mapping', CONF.identity_mapping.driver)
-        self.role_manager = manager.load_driver('keystone.role', CONF.role.driver)
+        conf_dir = CONF.identity.domain_config_dir
+        domain_config_file = "{}/keystone.{}.conf".format(
+            conf_dir, self.domain_name)
+        domain_config(args=[], project='keystone',
+                      default_config_files=[domain_config_file],
+                      default_config_dirs=[])
+        self.identity_manager = manager.load_driver(
+            'keystone.identity',
+            domain_config.identity.driver,
+            domain_config)
+
+    def _get_role_id(self):
+        role_name = CONF.json_assignment.default_roles[0]
         role_name_filter = driver_hints.Hints()
-        role_name_filter.add_filter('name', CONF.json_assignment.default_roles[0])
-        self.role_id = self.role_manager.list_roles(role_name_filter)[0]['id']
+        role_name_filter.add_filter('name', role_name)
+        return self.role_manager.list_roles(role_name_filter)[0]['id']
+
+    def _get_user_id(self, user_name):
+        user_id = self.identity_manager.get_user_by_name(
+            user_name, self.domain_name)['id']
+        user_id = self.id_mapping_manager.get_public_id({
+            'domain_id': self.domain_id,
+            'local_id': user_id,
+            'entity_type': 'user'})
+        return user_id
+
+    def __init__(self):
+        self.domain_name = CONF.json_assignment.ldap_domain_name
+        self._setup_managers()
+        self.domain_id = self.resource_manager.get_project_by_name(
+            self.domain_name, domain_id=None)['id']
+        self.role_id = self._get_role_id()
+
         with open('/etc/keystone/user-project-map.json', 'r') as f:
             self.userprojectmap = yaml.load(f)
         projectidcache = {}
@@ -70,7 +100,8 @@ class Assignment(sql.Assignment):
                             domain_id=None, project_id=None,
                             inherited_to_projects=False):
         """List role ids for assignments/grants."""
-        raise exception.NotImplemented(message="List Grant Role IDs not implemented yet")
+        msg = "List Grant Role IDs not implemented yet"
+        raise exception.NotImplemented(message=msg)
 
     def check_grant_role_id(self, role_id, user_id=None, group_id=None,
                             domain_id=None, project_id=None,
@@ -82,7 +113,8 @@ class Assignment(sql.Assignment):
         :returns: None or raises an exception if grant not found
 
         """
-        raise exception.NotImplemented(message="Check Grant Role ID not implemented yet")
+        msg = "Check Grant Role ID not implemented yet"
+        raise exception.NotImplemented(message=msg)
 
     def list_role_assignments(self, role_id=None,
                               user_id=None, group_ids=None,
@@ -100,40 +132,51 @@ class Assignment(sql.Assignment):
              inherited_to_projects=inherited_to_projects)
 
         for user, projects in self.userprojectmap.items():
-            user_id = self.identity_manager.get_user_by_name(user, self.domain_name)['id']
-            user_id = self.id_mapping_manager.get_public_id({'domain_id': self.domain_id, 'local_id': user_id, 'entity_type': 'user' })
+            user_id = self._get_user_id(user)
             for project in projects:
-                role_assignments.append({'role_id': self.role_id, 'user_id': user_id, 'project_id': project})
-
+                role_assignments.append({
+                    'role_id': self.role_id,
+                    'user_id': user_id,
+                    'project_id': project
+                })
         return role_assignments
 
     def add_role_to_user_and_project(self, user_id, tenant_id, role_id):
-        raise exception.NotImplemented(message="This assignment backend is read-only.")
+        msg = "This assignment backend is read-only."
+        raise exception.NotImplemented(message=msg)
 
     def remove_role_from_user_and_project(self, user_id, tenant_id, role_id):
-        raise exception.NotImplemented(message="This assignment backend is read-only.")
+        msg = "This assignment backend is read-only."
+        raise exception.NotImplemented(message=msg)
 
     def create_grant(self, role_id, user_id=None, group_id=None,
                      domain_id=None, project_id=None,
                      inherited_to_projects=False):
-        raise exception.NotImplemented(message="This assignment backend is read-only.")
+        msg = "This assignment backend is read-only."
+        raise exception.NotImplemented(message=msg)
 
     def delete_grant(self, role_id, user_id=None, group_id=None,
                      domain_id=None, project_id=None,
                      inherited_to_projects=False):
-        raise exception.NotImplemented(message="This assignment backend is read-only.")
+        msg = "This assignment backend is read-only."
+        raise exception.NotImplemented(message=msg)
 
     def delete_project_assignments(self, project_id):
-        raise exception.NotImplemented(message="This assignment backend is read-only.")
+        msg = "This assignment backend is read-only."
+        raise exception.NotImplemented(message=msg)
 
     def delete_role_assignments(self, role_id):
-        raise exception.NotImplemented(message="This assignment backend is read-only.")
+        msg = "This assignment backend is read-only."
+        raise exception.NotImplemented(message=msg)
 
     def delete_user_assignments(self, user_id):
-        raise exception.NotImplemented(message="This assignment backend is read-only.")
+        msg = "This assignment backend is read-only."
+        raise exception.NotImplemented(message=msg)
 
     def delete_group_assignments(self, group_id):
-        raise exception.NotImplemented(message="This assignment backend is read-only.")
+        msg = "This assignment backend is read-only."
+        raise exception.NotImplemented(message=msg)
 
     def delete_domain_assignments(self, domain_id):
-        raise exception.NotImplemented(message="This assignment backend is read-only.")
+        msg = "This assignment backend is read-only."
+        raise exception.NotImplemented(message=msg)
