3b3b965c13c4d0b5b39f3ff4f0537316f319a542
Jiří Suchomel <jiri.suchomel@suse.com>
2018-11-22 15:40:33 +0100

    Bump the master version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='keystone-json-assignment',
-    version='0.0.2',
+    version='0.0.3',
     description='JSON Backend for the Keystone Assignment Driver',
     url='https://github.com/SUSE-Cloud/keystone-json-assignment',
     author='Colleen Murphy',
